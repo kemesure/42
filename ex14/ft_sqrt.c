dafa1f23b112604a9bd0569002ac32c6d264feec
kevin <kmesureux@outlook.com>
2017-11-11 00:11:22 +0100

    ex14

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -10,18 +10,4 @@ int	ft_sqrt(int nb)
 		n++;
 	}
 	return 0;
-/*
-	int	racine;
-	int	sqrt;
-
-	racine = 1;
-	sqrt = 0;
-	while(racine <= nb / 2)
-	{
-		sqrt = racine * racine;
-		if(sqrt == nb)
-			return racine;
-		racine++;
-	}
-	return 0;
-*/}
+}
