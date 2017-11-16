faa2a7fcc2810ce092b72f234830caede34cf265
kemesure <kmesureux@outlook.com>
2017-11-16 19:10:24 +0100

    ex25

---
+++ ex25/ft_putnbr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 16:00:46 by kemesure          #+#    #+#             */
+/*   Updated: 2017/11/16 18:21:31 by kemesure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+void	ft_putnbr(int nb)
+{
+	if (nb < -2147483648 || nb > 2147483647)
+		return ;
+	if (nb == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+	return ;
+}
