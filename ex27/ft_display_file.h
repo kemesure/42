ce4171fc922f63621a5201d3ec5656aedc600aba
kemesure <kmesureux@outlook.com>
2017-11-17 02:34:00 +0100

    ex27

---
+++ ex27/ft_display_file.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 21:36:22 by kemesure          #+#    #+#             */
+/*   Updated: 2017/11/17 01:49:41 by kemesure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_DISPLAY_FILE_H__
+# define __FT_DISPLAY_FILE_H__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#endif
