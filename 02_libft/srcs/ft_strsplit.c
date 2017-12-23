183bfe8e43afcf86a4cce8dfa5cd11fb788147a7
kemesure <kmesureux@outlook.com>
2017-12-23 01:14:10 +0100

    libft

--- 02_libft/srcs/ft_strsplit.c
+++ 02_libft/srcs/ft_strsplit.c
@@ -6,23 +6,44 @@
 /*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 12:06:23 by kemesure          #+#    #+#             */
-/*   Updated: 2017/12/17 19:04:41 by kemesure         ###   ########.fr       */
+/*   Updated: 2017/12/23 01:11:49 by kemesure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+void	ft_allocation(char const *s, char c, char **tab)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	while (s[j])
+	{
+		while (s[j] && s[j] == c)
+			j++;
+		len = j;
+		while (s[len] && s[len] != c)
+			len++;
+		if (len != j)
+		{
+			tab[i] = (char *)malloc(len - j + 1);
+			if (tab[i] == NULL)
+				return (NULL);
+		}
+		i++;
+		j = len;
+	}
+}
+
+void	ft_assignment(char const *s, char c, char **tab)
 {
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	size_t	len;
-	char	**tab;
 
-	if (s == NULL)
-		return (NULL);
-	tab = (void *)0;
 	i = 0;
 	j = 0;
 	k = 0;
@@ -30,33 +51,27 @@ char	**ft_strsplit(char const *s, char c)
 	{
 		while (s[k] && s[k] == c)
 			k++;
-		len = k;
-		while (s[len] && s[len] != c)
-			len++;
-		if (s[len])
-		{
-			tab[i] = (char *)malloc(len - k + 1);
-			if (tab[i] == NULL)
-				return (NULL);
-		}
-		i++;
-		k = len;
-	}
-	k = 0;
-	while (s[k])
-	{
-		while (s[k] && s[k] == c)
-			k++;
 		i = 0;
-		while (s[k] && s[k] != c)
+		while (s[k + i] && s[k + i] != c)
 		{
-			tab[j][i] = s[k];
+			tab[j][i] = s[k + i];
 			i++;
-			k++;
 		}
-		if (s[k])
+		if (i)
 			tab[j][k] = '\0';
+		k += i;
 		j++;
 	}
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char	**tab;
+
+	if (s == NULL)
+		return (NULL);
+	tab = (void *)0;
+	ft_allocation(s, c, tab);
+	ft_assignment(s, c, tab);
 	return (tab);
 }
