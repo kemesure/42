d1d0962c099dd5fd030b32fee97f7d3f38820f71
Kevin MESUREUX <kemesure@e3r6p17.42.fr>
2017-12-29 14:04:07 +0100

    libft

--- 02_libft/srcs/ft_itoa.c
+++ 02_libft/srcs/ft_itoa.c
@@ -6,58 +6,52 @@
 /*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 20:00:46 by kemesure          #+#    #+#             */
-/*   Updated: 2017/12/28 16:35:24 by kemesure         ###   ########.fr       */
+/*   Updated: 2017/12/29 14:00:46 by kemesure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
-//------------------------------
-#include <stdio.h> // A RETIRER
-//------------------------------
-// n = 6, *len = 3, *sign = 1
+
 void	ft_intlen(int n, int *len, int *sign)
 {
-	printf("\tFT_INTLEN(int n %d, int *len %d, int *sign %d)\n", n, *len, *sign);
-	if (n == -2147483648) // FAUX
+	if (n == -2147483648)
 	{
 		*len = 11;
 		return ;
 	}
-	if (n < 0) // FAUX
+	if (n < 0)
 	{
-		n *= -1; // n = 623
-		*sign += 1; // sign = 1
+		n *= -1;
+		*sign += 1;
 	}
-	if (n == 0) // FAUX
+	if (n == 0)
 		*len = 1;
-	else if (n > 999999999) // FAUX
+	else if (n > 999999999)
 		*len = 10;
-	else // VRAI
+	else
 	{
-		if (n / 10 != 0) // 6 / 10 = 0 donc FAUX
+		if (n / 10 != 0)
 		{
-			len++;
+			*len += 1;
 			ft_intlen(n / 10, len, sign);
 		}
 	}
 }
-// n = -623, *str, sign = 1, len = 3
+
 void	ft_itoa_assignment(int n, char *str, int sign, int len)
 {
-	printf("\tFT_ITOA_ASSIGNMENT(int n %d, char *str \"%s\", int sign %d, int len %d)\n", n, str, sign, len);
 	int		i;
 
 	i = 0;
-	if (sign == 1) // VRAI
+	if (sign == 1)
 	{
-		str[i] = '-'; // str = "-"
-		n *= -1; // n = 623
-		i++; // i = 1
-		while (i <= len) // 4 !<= 3 donc FAUX
+		str[i] = '-';
+		n *= -1;
+		i++;
+		while (i <= len)
 		{
-			// str = "-623"
 			str[i] = (n / ft_puiss(10, len - i)) % 10 + '0';
-			i++; // i = 4
+			i++;
 		}
 	}
 	else
@@ -68,25 +62,23 @@ void	ft_itoa_assignment(int n, char *str, int sign, int len)
 			i++;
 		}
 	}
-	str[i] = '\0'; // str = "-623\0"
+	str[i] = '\0';
 }
-// n = -623
+
 char	*ft_itoa(int n)
 {
-	printf("\nFT_ITOA(%d)\n", n);
 	char	*str;
 	int		len;
 	int		sign;
 
 	len = 1;
 	sign = 0;
-	if (n == -2147483648) // FAUX
+	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	ft_intlen(n, &len, &sign); // len = 3, sign = 1
-	printf("n = %d, len = %d, sign = %d\n", n, len, sign);
-	str = malloc(len + 1 + sign); // str = 5 octets
+	ft_intlen(n, &len, &sign);
+	str = malloc(len + 1 + sign);
 	if (str == NULL)
 		return (NULL);
-	ft_itoa_assignment(n, str, sign, len); // str = "-623\0"
+	ft_itoa_assignment(n, str, sign, len);
 	return (str);
 }
