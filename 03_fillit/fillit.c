1f88cc54e4c67a2844fbffdc01771294d7ca6ce4
Kevin MESUREUX <kemesure@e3r11p22.42.fr>
2018-01-29 15:53:11 +0100

    fillit debut

---
+++ 03_fillit/fillit.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/22 14:35:35 by kemesure          #+#    #+#             */
+/*   Updated: 2018/01/27 17:04:41 by kemesure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	DESCRIPTION :
+**	Sorte de Tetris, qui doit faire le plus petit carre possible avec des pieces
+**	de Tetriminos passees en argument dans un fichier.
+**
+**	ERRORS :
+**	Il ne doit y avoir qu'un seul argument, sinon le programme affiche son usage
+**	et quitte proprement.
+**	Les pieces de Tetriminos dans le fichier passe en argument doivent toutes
+**	etre valides, sinon le programme affiche "error" et quitte proprement.
+*/
+
+#include "fillit.h"
+
+int		main(int ac, char **av)
+{
+	int		fd;
+
+	fd = open("tetriminos.txt", O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	if (ac != 2)
+	{
+		write(1, "usage: ./fillit file\n", 21);
+		return (NULL);
+	}
+	if (ft_check_valid_file(fd) == -1)
+	{
+		write(1, "error\n", 6);
+		return (NULL);
+	}
+	ft_place_tetriminos(fd);
+	ft_print_tetriminos(fd);
+	close(fd);
+	return (0);
+}
---
+++ 03_fillit/fillit.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/23 16:40:24 by kemesure          #+#    #+#             */
+/*   Updated: 2018/01/26 13:33:03 by kemesure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+/*
+**	pour open
+*/
+#include <fcntl.h>
+/*
+**	pour read, write et close
+*/
+#include <unistd.h>
+/*
+**	pour malloc et free
+*/
+#include <stdlib.h>
+#include "./libft/libft.h"
+
+	ft_read_file();
+int		ft_check_valid_file(int fd);
+void	ft_place_tetriminos(int fd);
+void	ft_print_tetriminos(int fd);
+
+struct		s_coord
+{
+	int		x[4];
+	int		y[4];
+};
+
+#endif
