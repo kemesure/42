26d3082ca7c6332dcc08264649de0b4f00c31890
Kevin MESUREUX <kemesure@e1r10p22.42.fr>
2018-01-30 18:51:45 +0100

    gnl

--- 04_get_next_line/get_next_line.c
+++ 04_get_next_line/get_next_line.c
@@ -6,15 +6,41 @@
 /*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 17:25:12 by kemesure          #+#    #+#             */
-/*   Updated: 2018/01/29 17:56:04 by kemesure         ###   ########.fr       */
+/*   Updated: 2018/01/30 18:49:23 by kemesure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
 int		get_next_line(const int fd, char **line)
 {
+	int				i;
+	char			*ret;
+//	char			**ptr;
+	char			*buff;
+
+	i = 0;
+	while (read(fd, buff, BUFF_SIZE)) // vrai tant que caractere lu != '\0'
+	{
+		printf("i = %d\n", i);
+		ret = (char *)malloc(BUFF_SIZE); // ret = 32 octets
+		if (ret == NULL)
+		{
+			return (-1);
+		}
+		ret = buff;
+//		ptr = &ret;
+		if (*buff == '\n')
+		{
+			return (1);
+		}
+		else if (*buff == '\0')
+		{
+			return (42);
+		}
+		i++;
+	}
+	return (21);
 }
 
 // ___A RETIRER___
@@ -24,15 +50,16 @@ int		get_next_line(const int fd, char **line)
 int		main(int ac, char **av)
 {
 	int		fd;
-	char	*c;
+	char	*buff;
 
 	if (ac != 2)
 	{
 		write(1, "usage: ./a.out file\n", 20);
 		return (NULL);
 	}
-	fd = open(av[1], O_RDONLY);
+	fd = open(av[1], O_RDONLY); // open "texte.txt"
 	if (fd == -1)
 		return (NULL);
-	printf("GNL return : %d", get_next_line(fd, &c));
+	printf("GNL return : %d", get_next_line(fd, &buff));
+	return (0);
 }
--- 04_get_next_line/get_next_line.h
+++ 04_get_next_line/get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: kemesure <kemesure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 17:25:15 by kemesure          #+#    #+#             */
-/*   Updated: 2018/01/29 17:35:19 by kemesure         ###   ########.fr       */
+/*   Updated: 2018/01/30 13:35:01 by kemesure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+/*
+**	Fonctions autorisees : read, malloc et free
+*/
+# include <fcntl.h>
+# include <unistd.h>
+# define BUFF_SIZE 32
 
+#endif
